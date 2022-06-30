c71bf8ba1c90987cd9f8558100664eccec9c17c2
zhangyiwen <zzyyyww9595@gmail.com>
2022-06-30 10:28:33 +0800

    add script for run test

--- run-tpds.sh
+++ run-tpds.sh
@@ -12,16 +12,18 @@ workloads=(
 )
 
 rm -f ycsbc.output
-
-for db in ${dbs[@]} ;
-do
-  rm -rf /mnt/pmem1/*
-
-  for workload in ${workloads[@]};
+for tn in 32
   do
-    echo "Run $db workload-$workload with threads 1 ratio 20" >> "ycsbc.output"
-    cmd="numactl -N 1 ./cmake-build-release-2373-physic/ycsb -db $db -threads 1 -P workloads/workload-$workload.spec -file_ratio 20 >> "ycsbc.output""
-    #echo $cmd
-    eval $cmd
+  for db in ${dbs[@]} ;
+  do
+    rm -rf /mnt/pmem1/*
+
+    for workload in ${workloads[@]};
+    do
+      echo "Run $db workload-$workload with threads $tn ratio 20" >> "ycsbc.output"
+      cmd="numactl -N 1 ./cmake-build-release-2373-physic/ycsb -db $db -threads 1 -P workloads/workload-$workload.spec -file_ratio 20 >> "ycsbc.output""
+      #echo $cmd
+      eval $cmd
+    done
   done
 done
